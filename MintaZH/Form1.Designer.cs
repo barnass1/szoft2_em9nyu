947b09cb21ce55965e04fff4064194f4ee3f48af
Barnabás Medve <barnabas.medve@stud.uni-corvinus.hu>
2024-11-06 11:52:23 +0100

    ZH

--- MintaZH/Form1.Designer.cs
+++ MintaZH/Form1.Designer.cs
@@ -28,12 +28,194 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            components = new System.ComponentModel.Container();
+            listBox1 = new ListBox();
+            fogasokBindingSource = new BindingSource(components);
+            nyersanyagokBindingSource = new BindingSource(components);
+            listBox2 = new ListBox();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            button2 = new Button();
+            dataGridView1 = new DataGridView();
+            nyersanyagNevDataGridViewTextBoxColumn = new DataGridViewTextBoxColumn();
+            hozzávalóBindingSource = new BindingSource(components);
+            button1 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            textBox3 = new TextBox();
+            label1 = new Label();
+            ((System.ComponentModel.ISupportInitialize)fogasokBindingSource).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)nyersanyagokBindingSource).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)hozzávalóBindingSource).BeginInit();
+            SuspendLayout();
+            // 
+            // listBox1
+            // 
+            listBox1.DataSource = fogasokBindingSource;
+            listBox1.DisplayMember = "FogasNev";
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 15;
+            listBox1.Location = new Point(12, 44);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(185, 364);
+            listBox1.TabIndex = 0;
+            listBox1.SelectedIndexChanged += listBox1_SelectedIndexChanged;
+            // 
+            // fogasokBindingSource
+            // 
+            fogasokBindingSource.DataSource = typeof(Models.Fogasok);
+            // 
+            // nyersanyagokBindingSource
+            // 
+            nyersanyagokBindingSource.DataSource = typeof(Models.Nyersanyagok);
+            // 
+            // listBox2
+            // 
+            listBox2.DataSource = nyersanyagokBindingSource;
+            listBox2.DisplayMember = "NyersanyagNev";
+            listBox2.FormattingEnabled = true;
+            listBox2.ItemHeight = 15;
+            listBox2.Location = new Point(603, 44);
+            listBox2.Name = "listBox2";
+            listBox2.Size = new Size(185, 364);
+            listBox2.TabIndex = 1;
+            listBox2.SelectedIndexChanged += listBox2_SelectedIndexChanged_1;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(12, 12);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(185, 23);
+            textBox1.TabIndex = 2;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(603, 12);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(185, 23);
+            textBox2.TabIndex = 3;
+            textBox2.TextChanged += textBox2_TextChanged;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(122, 415);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 5;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.AutoGenerateColumns = false;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { nyersanyagNevDataGridViewTextBoxColumn });
+            dataGridView1.DataSource = hozzávalóBindingSource;
+            dataGridView1.Location = new Point(203, 44);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowTemplate.Height = 25;
+            dataGridView1.Size = new Size(333, 364);
+            dataGridView1.TabIndex = 6;
+            // 
+            // nyersanyagNevDataGridViewTextBoxColumn
+            // 
+            nyersanyagNevDataGridViewTextBoxColumn.DataPropertyName = "NyersanyagNev";
+            nyersanyagNevDataGridViewTextBoxColumn.HeaderText = "NyersanyagNev";
+            nyersanyagNevDataGridViewTextBoxColumn.Name = "nyersanyagNevDataGridViewTextBoxColumn";
+            // 
+            // hozzávalóBindingSource
+            // 
+            hozzávalóBindingSource.DataSource = typeof(Hozzávaló);
+            // 
+            // button1
+            // 
+            button1.Location = new Point(12, 415);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 7;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(542, 212);
+            button3.Name = "button3";
+            button3.Size = new Size(55, 23);
+            button3.TabIndex = 8;
+            button3.Text = "+";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(542, 255);
+            button4.Name = "button4";
+            button4.Size = new Size(55, 23);
+            button4.TabIndex = 9;
+            button4.Text = "-";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(603, 415);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(100, 23);
+            textBox3.TabIndex = 10;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(709, 419);
+            label1.Name = "label1";
+            label1.Size = new Size(38, 15);
+            label1.TabIndex = 11;
+            label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label1);
+            Controls.Add(textBox3);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button1);
+            Controls.Add(dataGridView1);
+            Controls.Add(button2);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(listBox2);
+            Controls.Add(listBox1);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)fogasokBindingSource).EndInit();
+            ((System.ComponentModel.ISupportInitialize)nyersanyagokBindingSource).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)hozzávalóBindingSource).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private ListBox listBox1;
+        private ListBox listBox2;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Button button2;
+        private DataGridView dataGridView1;
+        private Button button1;
+        private Button button3;
+        private Button button4;
+        private TextBox textBox3;
+        private Label label1;
+        private DataGridViewTextBoxColumn nyersanyagNevDataGridViewTextBoxColumn;
+        private BindingSource nyersanyagokBindingSource;
+        private BindingSource fogasokBindingSource;
+        private BindingSource hozzávalóBindingSource;
     }
 }
--- MintaZH/Form1.cs
+++ MintaZH/Form1.cs
@@ -1,10 +1,147 @@
+using MintaZH.Models;
+
 namespace MintaZH
 {
     public partial class Form1 : Form
     {
+        private ReceptContext _context;
+
+
+
         public Form1()
         {
             InitializeComponent();
+            _context = new ReceptContext();
+            NyersanyagListazas();
+            FogasListazas();
         }
+
+        private void FogasListazas()
+        {
+            var hv = from x in _context.Fogasok
+                     where x.FogasNev.Contains(textBox2.Text)
+                     select x;
+
+            fogasokBindingSource.DataSource = hv.ToList();
+        }
+
+        private void NyersanyagListazas()
+        {
+            var hv = from x in _context.Nyersanyagok
+                     where x.NyersanyagNev.Contains(textBox1.Text)
+                     select x;
+
+            nyersanyagokBindingSource.DataSource = hv.ToList();
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            string filterString = textBox1.Text.ToLower();
+            var filteredData = _context.Nyersanyagok
+                                       .Where(x => x.NyersanyagNev.ToLower().Contains(filterString))
+                                       .ToList();
+            nyersanyagokBindingSource.DataSource = filteredData;
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            string filterString = textBox2.Text.ToLower();
+            var filteredData = _context.Nyersanyagok
+                                       .Where(x => x.NyersanyagNev.ToLower().Contains(filterString))
+                                       .ToList();
+            nyersanyagokBindingSource.DataSource = filteredData;
+        }
+
+        private void listBox2_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+            if (listBox2.SelectedItem is Nyersanyagok kiválasztottNyersanyag)
+            {
+                var megyseg = (from x in _context.MennyisegiEgysegek
+                               where x.MennyisegiEgysegId == kiválasztottNyersanyag.MennyisegiEgysegId
+                               select x).FirstOrDefault();
+                if (megyseg != null)
+                {
+                    label1.Text = megyseg.EgysegNev;
+                }
+                else
+                {
+                    label1.Text = "N/A"; // or handle the null case appropriately
+                }
+            }
+            else
+            {
+                label1.Text = "N/A"; // or handle the null case appropriately
+            }
+        }
+
+        private void HozzávalóListázás()
+        {
+            if (listBox1.SelectedItem == null)
+            {
+                // Handle the case when no item is selected
+                return;
+            }
+
+            var id = ((Fogasok)listBox1.SelectedItem).FogasId;
+
+            var hozzávalók = from x in _context.Receptek
+                             where x.FogasId == id
+                             select new Hozzávaló
+                             {
+                                 ReceptID = x.ReceptId,
+                                 FogasID = x.FogasId,
+                                 NyersanyagNev = x.Nyersanyag.NyersanyagNev,
+                                 Mennyiseg_4fo = x.Mennyiseg4fo,
+                                 EgysegNev = x.Nyersanyag.MennyisegiEgyseg.EgysegNev,
+                                 Ár = x.Mennyiseg4fo * (double?)x.Nyersanyag.Egysegar
+                             };
+            hozzávalóBindingSource.DataSource = hozzávalók.ToList();
+        }
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Receptek r = new Receptek();
+            r.NyersanyagId = ((Nyersanyagok)listBox2.SelectedItem).NyersanyagId;
+            r.FogasId = ((Fogasok)listBox1.SelectedItem).FogasId;
+            double m;
+            if (!double.TryParse(textBox3.Text, out m)) return;
+            r.Mennyiseg4fo = m;
+            _context.Receptek.Add(r);
+            _context.SaveChanges();
+            HozzávalóListázás(); //Az középsõ rácsot kell újratölteni
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            HozzávalóListázás();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            var selectedHozzávaló = (Hozzávaló)hozzávalóBindingSource.Current;
+
+            var törlendõ = (from x in _context.Receptek
+                            where x.ReceptId == selectedHozzávaló.ReceptID
+                            select x).FirstOrDefault();
+
+            if (törlendõ != null)
+            {
+                _context.Receptek.Remove(törlendõ);
+                _context.SaveChanges();
+                HozzávalóListázás();
+            }
+        }
+    }
+
+    public class Hozzávaló
+    {
+        public int ReceptID { get; set; }
+        public int? FogasID { get; set; }
+        public string NyersanyagNev { get; set; }
+        public double? Mennyiseg_4fo { get; set; }
+        public string EgysegNev { get; set; }
+        public double? Ár { get; set; }
     }
 }
