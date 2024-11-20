ed30352e448fd6e1877e3e9ca38dfea51197a9f1
barnass1 <barni.medve@icloud.com>
2024-11-20 16:39:46 +0100

    gyak10

--- webApp/Controllers/viccController.cs
+++ webApp/Controllers/viccController.cs
@@ -7,37 +7,45 @@ namespace webApp.Controllers
 {
     [Route("api/vicci")]
     [ApiController]
-    public class viccController : ControllerBase
+    public class ViccController : ControllerBase
     {
         // GET: api/<ValuesController>
-        [Route("{id}")]
+        //[Route("{id}")]
         [HttpGet]
         public IActionResult Get(int id)
         {
             FunnyDatabaseContext context = new FunnyDatabaseContext();
-            var v = from x in context.Jokes
-                    where x.JokeSk == id
-                    select x;
-
-            return Ok(v);
+            return Ok(context.Jokes.ToList());
         }
+
+
 
         // POST api/<ValuesController>
         [HttpPost]
-        public void Post([FromBody] string value)
+        public void Post([FromBody] Joke újVicc)
         {
+            FunnyDatabaseContext context = new FunnyDatabaseContext();
+            context.Jokes.Add(újVicc);
+            context.SaveChanges();
         }
 
         // PUT api/<ValuesController>/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody] string value)
         {
+
         }
 
         // DELETE api/<ValuesController>/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
+            FunnyDatabaseContext context = new FunnyDatabaseContext();
+            var törlendőVicc = (from x in context.Jokes
+                                where x.JokeSk == id
+                                select x).FirstOrDefault();
+            context.Remove(törlendőVicc);
+            context.SaveChanges();
         }
     }
 }
